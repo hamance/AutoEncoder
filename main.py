2d9da5493edc530b0a390c29eb1bc11c8779fdec
hamance <htpln@foxmail.com>
2018-03-23 08:35:25 +0800

    .

--- main.py
+++ main.py
@@ -28,16 +28,19 @@ def main(**kwargs):
     dataloader = get_loader(opt)
 
     for epoch in range(opt.num_epochs):
-        for data in dataloader:
+        for ii, data in enumerate(dataloader):
             img, _ = data
             img = Variable(img).cuda()
 
             output = model(img)
             loss = criterion(output, img)
 
+            optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            et = time.time()
+
+            if ii % 100 == 0:
+                print("step [{}:{}], loss {:.4f}".format(epoch+1, ii, loss.data[0]))
 
         print("epoch [{}/{}], loss{:.4f}".format(epoch+1, opt.num_epochs, loss.data[0]))
 
